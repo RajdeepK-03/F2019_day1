9533fe9dcaa65f9e834a1ba654d7d3203dcd93f1
RajdeepK-03 <chahal.rajdeep03@gmail.com>
2019-09-18 19:21:32 -0400

    [Rajdeep] comment added

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -5,10 +5,15 @@ public class FirstDay
     public static void main(String[] args)
     {
         System.out.println("Rajdeep");
+
+        // Variable declaration
         int a, b, c;
+
+        // Value Assignment
         a = 10;
         b = 20;
         c = a + b;
+        // Display result
         System.out.println("Sum : " + c);
     }
 }
