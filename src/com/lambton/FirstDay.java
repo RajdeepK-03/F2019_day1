e569363e02222ed2b64360a5e95f1791dbf34328
RajdeepK-03 <chahal.rajdeep03@gmail.com>
2019-09-18 20:36:53 -0400

    [Rajdeep] Student data added

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -15,5 +15,15 @@ public class FirstDay
         c = a + b;
 
         System.out.println("Sum : " + c);
+
+        int x = Integer.MAX_VALUE;
+        System.out.println( "X : " + x);
+        int y = x + 1;
+        System.out.println("Y :" + y);
+        System.out.println("Min :" + Integer.MIN_VALUE);
+
+        student s1 = new student();
+        s1.setValues();
+        s1.display();
     }
 }
--- src/com/lambton/student.java
+++ src/com/lambton/student.java
@@ -4,4 +4,15 @@ public class student
 {
     int studentID;
     String studentName;
+
+    void setValues()
+    {
+        studentID = 1;
+        studentName = "Rajdeep";
+    }
+    void display()
+    {
+        System.out.println("student ID :" + studentID);
+        System.out.println("Student Name :" + studentName);
+    }
 }
